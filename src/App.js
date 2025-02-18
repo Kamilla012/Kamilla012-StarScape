ab37bdc8b4d7b1807cb1ece071532381b91f0381
Kamilla <kamilladrozdz02@gmail.com>
2025-02-18 22:04:58 +0000

    second commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Button from "./components/Button";
+import ButtonGradient from "../src/assets/ButtonGradient";
+import Header from "./components/Header";
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hiddenn">
+        <ButtonGradient />
+
+        <Header />
+      </div>
+    </>
+  );
+};
+export default App;
